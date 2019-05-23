72bb28a57407f67a3bb99ba67a494e14d62330ac
yukikom-biz <yuki.komatsu@bizreach.co.jp>
2019-05-23 15:38:16 +0900

    add: comment for todo

--- src/controller/ItemClass.java
+++ src/controller/ItemClass.java
@@ -1,19 +1,28 @@
 package controller;
 
+import common.DAOException;
+import model.ItemBean;
+import model.ItemDAO;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet("/ItemClass")
 public class ItemClass extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        //todo: if the method is post, find by keyword. and show items.
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        //todo: if the method is get, show the top page.
     }
+
 }
+
