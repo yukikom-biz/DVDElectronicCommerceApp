184ed556637a1da7d8b51d3e1f6e06f24b59ace1
yukikom-biz <yuki.komatsu@bizreach.co.jp>
2019-05-23 13:46:52 +0900

    add: necessary function (name only)

--- src/model/Item.java
+++ src/model/Item.java
@@ -1,6 +1,18 @@
 package model;
 
+import java.util.List;
+
 public class Item {
 
+    public static List getList(String keyword){
 
+    }
+    
+    public static List getList(String keyword,int page ){
+
+    }
+
+    public static Item getItem(){
+
+    }
 }
