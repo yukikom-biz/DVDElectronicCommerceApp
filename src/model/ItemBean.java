2ea2fd5f836e7dd146134bbf7ac8116a4c1ada9f
yukikom-biz <yuki.komatsu@bizreach.co.jp>
2019-05-23 13:14:56 +0900

    create ItemBeans

---
+++ src/model/ItemBean.java
@@ -0,0 +1,99 @@
+package model;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.Map;
+
+public class ItemBean implements Serializable {
+    private int id, price;
+    private String title,players,directors;
+    private Timestamp updated,created;
+    private Map validation;
+
+    //This constructor is in accordance with textbook.
+    public ItemBean(Map RequestParams) {
+        this.id = (int) RequestParams.get("id");
+        this.price = (int) RequestParams.get("price");
+        this.title = (String) RequestParams.get("title");
+        this.players = (String) RequestParams.get("players");
+        this.directors = (String) RequestParams.get("directors");
+        this.updated = (Timestamp) RequestParams.get("updated");
+        this.created = (Timestamp) RequestParams.get("created");
+        this.validation = (Map<String, String>) RequestParams.get("validation");
+    }
+
+    public ItemBean(int id, int price, String title, String players, String directors, Timestamp updated, Timestamp created, Map validation) {
+        this.id = id;
+        this.price = price;
+        this.title = title;
+        this.players = players;
+        this.directors = directors;
+        this.updated = updated;
+        this.created = created;
+        this.validation = validation;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(String players) {
+        this.players = players;
+    }
+
+    public String getDirectors() {
+        return directors;
+    }
+
+    public void setDirectors(String directors) {
+        this.directors = directors;
+    }
+
+    public Timestamp getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Timestamp updated) {
+        this.updated = updated;
+    }
+
+    public Timestamp getCreated() {
+        return created;
+    }
+
+    public void setCreated(Timestamp created) {
+        this.created = created;
+    }
+
+    public Map getValidation() {
+        return validation;
+    }
+
+    public void setValidation(Map validation) {
+        this.validation = validation;
+    }
+}
