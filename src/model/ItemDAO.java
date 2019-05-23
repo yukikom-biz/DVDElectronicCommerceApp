df9fc8571507fd51442d6fd2f5d15e139cf67a45
yukikom-biz <yuki.komatsu@bizreach.co.jp>
2019-05-23 13:24:57 +0900

    add: ItemDAO

--- src/model/ItemDAO.java
+++ src/model/ItemDAO.java
@@ -1,4 +1,10 @@
 package model;
 
+import java.sql.Connection;
+
 public class ItemDAO {
+    private Connection con;
+//    public ItemDAO() throws DAOException{
+//        getConnection();
+//    }
 }
