bec6382e82f0e1650ce9087a176c75e297e40f8e
yukikom-biz <yuki.komatsu@bizreach.co.jp>
2019-05-23 13:42:08 +0900

    add: ItemDAO for  Basic connention to Database

--- src/model/ItemDAO.java
+++ src/model/ItemDAO.java
@@ -1,10 +1,46 @@
 package model;
 
+import common.DAOException;
+
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ItemDAO {
-    private Connection con;
-//    public ItemDAO() throws DAOException{
-//        getConnection();
-//    }
+
+    private static final String DRIVER_CLASS_NAME = "com.mysql.cj.jdbc.Driver";
+    private static final String MYSQL_URL = "jdbc:mysql://localhost:3306/";
+    private static final String MYSQL_USERNAME = "user_name";
+    private static final String MYSQL_PASSWORD = "password";
+
+    private Connection conn;
+    public ItemDAO() throws DAOException {
+        getConnection();
+    }
+
+    public void close() throws DAOException {
+        if (null != conn) {
+            try {
+                conn.close();
+                conn = null;
+            } catch (SQLException ex) {
+//                throw new DAOException("Failed to close connection of database.", ex);
+            }
+        }
+    }
+
+    private void getConnection() throws DAOException {
+
+        try {
+
+            Class.forName(DRIVER_CLASS_NAME);
+            conn = DriverManager.getConnection(MYSQL_URL, MYSQL_USERNAME, MYSQL_PASSWORD);
+
+        } catch (ClassNotFoundException | SQLException ex) {
+//            throw new DAOException("Failed to connect to database.", ex);
+        }
+
+    }
+
+
 }
