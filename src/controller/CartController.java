cfdc068f773d009dcb58ad3bb7ca564ceaa0167b
Yin Cheng <yin.cheng@bizreach.co.jp>
2019-05-24 14:18:22 +0900

    CartController fixing.

--- src/controller/CartController.java
+++ src/controller/CartController.java
@@ -6,13 +6,14 @@ import model.ItemDAO;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet('/CartController')
+@WebServlet("/CartController")
 public class CartController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
@@ -28,14 +29,16 @@ public class CartController extends HttpServlet {
                     cart = new CartBean();
                     session.setAttribute("cart", cart);
                 }
-                ItemDAO dao = new ItemDAO();
-                ItemBean bean = new dao.findItem(id);
+                ItemBean bean = (new ItemDAO()).findItem(id);
                 cart.addCart(bean, quantity);
                 gotoPage(request, response, "/cart.jsp");
             } else if (mode.equals("2")) {
                 HttpSession session = request.getSession(true);
                 CartBean cart = (CartBean) session.getAttribute("cart");
                 int id = Integer.parseInt(request.getParameter("id"));
+                int quantity = Integer.parseInt(request.getParameter("quantity"));
+                ItemBean bean = (new ItemDAO()).findItem(id);
+                bean.setQuantity(quantity);
                 request.setAttribute("message", "Please do it properly.");
                 gotoPage(request, response, "/cart.jsp");
             } else if (mode.equals("3")) {
