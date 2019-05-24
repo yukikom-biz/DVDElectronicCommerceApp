254371fee1828270379c7d523d3bfb731980a007
Yin Cheng <yin.cheng@bizreach.co.jp>
2019-05-24 11:33:06 +0900

    OrderDAO

--- src/model/OrderDAO.java
+++ src/model/OrderDAO.java
@@ -3,4 +3,10 @@ package model;
 public class OrderDAO{
     private Connection con;
 
+    public OrderDAO() throws DAOException{
+        getConnection();
+    }
+
+    public int saveOrder(Cust)
+
 }
